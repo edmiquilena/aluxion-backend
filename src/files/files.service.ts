1fd7379ec5bac3ec5aef7804d0568852dcff3b25
Eduardo Miquilena <edumqr1@gmail.com>
2022-06-15 07:39:40 -0400

    refactor: files and s3 module structure

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,13 +1,8 @@
-import { isURL } from 'class-validator';
-import ValidatorJS  from 'validator';
-import { Injectable, NotAcceptableException } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { CreateFileDto } from './dto/create-file.dto';
 import { UpdateFileDto } from './dto/update-file.dto';
-import S3 from 'aws-sdk/clients/s3';
-import { AWSConnection } from 'src/lib/interfaces/general.interfaces';
 import { S3Service } from 'src/s3/s3.service';
-import { UploadFileDto } from './dto/upload-file.dto';
 import { S3UploadDto } from 'src/s3/dto/s3.upload.dto';
 import { FileDocument } from './schema/file.schema';
 import { InjectModel } from '@nestjs/mongoose';
@@ -15,44 +10,61 @@ import { Model } from 'mongoose';
 import { CopyFileDto } from './dto/copy-file.dto';
 import { unsplashQueryFileDto } from './dto/unsplash-file.dto';
 import { createApi } from 'unsplash-js';
-import * as nodeFetch from 'node-fetch'
-
+import * as nodeFetch from 'node-fetch';
 
 @Injectable()
 export class FilesService {
-  constructor( private configService: ConfigService,
+  constructor(
+    private configService: ConfigService,
     private readonly S3Service: S3Service,
-    @InjectModel('file') private fileModule: Model<FileDocument>) {}
+    @InjectModel('file') private fileModule: Model<FileDocument>,
+  ) {}
 
-  create(createFileDto: CreateFileDto) {
-    return 'This action adds a new file';
-  }
+
   async uploadFile(uploadData: S3UploadDto) {
-    console.log(uploadData)
-    const {userId} = uploadData;
-    console.log(userId)
-const fileUpload = await this.S3Service.uploadFile(uploadData);
-const {Location, Key} = fileUpload;
-const createEntry = new this.fileModule({creator: userId, location: Location, key: Key})
-createEntry.save();
+    console.log(uploadData);
+    const { userId } = uploadData;
+    console.log(userId);
+    const fileUpload = await this.S3Service.uploadFile(uploadData);
+    const { Location, Key } = fileUpload;
+    const createEntry = new this.fileModule({
+      creator: userId,
+      location: Location,
+      key: Key,
+    });
+    createEntry.save();
 
-return {
-  Location, Key, id: createEntry._id
-}
+    return {
+      Location,
+      Key,
+      id: createEntry._id,
+    };
+  }
 
-}
+  async copyFile(copyData: CopyFileDto) {
+    const UnsplashAPI = createApi({
+      accessKey: process.env.UNSPLASH_ACCESS_TOKEN,
+      fetch: nodeFetch.default as unknown as typeof fetch,
+    });
+    const getUnsplashImage = await UnsplashAPI.photos.get({
+      photoId: copyData.id,
+    });
+    if (!getUnsplashImage.response)
+      throw new NotFoundException('ID does not match any photo in unsplash');
+    const getImage = await nodeFetch.default(
+      getUnsplashImage.response.urls.regular,
+    );
 
+    const dataBuffer = Buffer.from(await getImage.arrayBuffer());
+    // * Unsplash specifies that images uploaded on their service are converted to JPEG format.
+    const filename = `unsplash-${copyData.id}.jpg`;
 
-async copyFile(copyData: CopyFileDto) {
-console.log(copyData)
-if(!ValidatorJS.isURL(copyData.url)) throw new NotAcceptableException('Not a valid URL.');
-}
-
+    return this.uploadFile({ dataBuffer, userId: copyData.userId, filename });
+  }
 
   async searchUnsplash(query: unsplashQueryFileDto) {
-   
     const UnsplashAPI = createApi({
-      accessKey: 'phUvXxIs4jO6WH_eVmkwA-hmtSAzFLnCORzdry6m1CY',
+      accessKey: process.env.UNSPLASH_ACCESS_TOKEN,
       fetch: nodeFetch.default as unknown as typeof fetch,
     });
     const unsplashResult = await UnsplashAPI.search.getPhotos(query);
@@ -60,7 +72,6 @@ if(!ValidatorJS.isURL(copyData.url)) throw new NotAcceptableException('Not a val
   }
 
   async findAll() {
-
     return `This action returns all files`;
   }
 
